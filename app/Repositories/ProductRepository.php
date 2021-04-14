a87291cebc73096db21d21f050043340e2d131e7
suhasish86 <suhasish86@gmail.com>
2021-04-15 00:17:34 +0530

    check 27

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -9,15 +9,18 @@ class ProductRepository implements ProductRepositoryInterface
     public function get_collection()
     {
         $our_collection = Product::where('status', 1)
-                                    ->limit(6)
-                                    ->with('galleries')
-                                    ->get();
-        foreach($our_collection as $product){
+            ->whereIn('genre', ['frames', 'sunglass'])
+            ->limit(6)
+            ->with('galleries')
+            ->get();
+        foreach ($our_collection as $product) {
             $gallery = $product->galleries;
             $cnt = 1;
-            foreach($product->galleries as $image){
-                if($cnt == 1)
-                $product->image = '/storage/uploads/gallery/'.$product->genre.'/'.$image->image;
+            foreach ($product->galleries as $image) {
+                if ($cnt == 1) {
+                    $product->image = '/storage/uploads/gallery/' . $product->genre . '/' . $image->image;
+                }
+
                 $cnt++;
             }
         }
@@ -28,16 +31,19 @@ class ProductRepository implements ProductRepositoryInterface
     public function get_newest()
     {
         $newest_arrival = Product::where('status', 1)
-                                    ->orderBy('created_at', 'desc')
-                                    ->limit(9)
-                                    ->with('galleries')
-                                    ->get();
-        foreach($newest_arrival as $product){
+            ->whereIn('genre', ['frames', 'sunglass'])
+            ->orderBy('created_at', 'desc')
+            ->limit(9)
+            ->with('galleries')
+            ->get();
+        foreach ($newest_arrival as $product) {
             $gallery = $product->galleries;
             $cnt = 1;
-            foreach($product->galleries as $image){
-                if($cnt == 1)
-                $product->image = '/storage/uploads/gallery/'.$product->genre.'/'.$image->image;
+            foreach ($product->galleries as $image) {
+                if ($cnt == 1) {
+                    $product->image = '/storage/uploads/gallery/' . $product->genre . '/' . $image->image;
+                }
+
                 $cnt++;
             }
         }
