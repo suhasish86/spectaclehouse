3ad327b3f0fa7e9a375ed770b9ca6f658659a80e
suhasish86 <suhasish86@gmail.com>
2021-04-15 01:59:52 +0530

    check 41

--- resources/views/site/eyeclinic.blade.php
+++ resources/views/site/eyeclinic.blade.php
@@ -14,11 +14,11 @@
         @foreach($eye_clinic as $facility)
             <div class="row align-items-center mb-5">
                 <div class="col-md-6" {{ (($loop->iteration%2) == 0) ? 'order-md-2' : '' }}>
-                    <div class="mb-2"><img src="{{ asset('siteassets/img/lservices.png') }}" alt=""></div>
+                    <div class="mb-2"><img src="{{ asset('siteassets/img/services.png') }}" alt=""></div>
                 </div>
-                <div class="col-md-6 {{ (($loop->iteration%2) == 0) ? 'order-md-1' : '' }}">
+                <div class="col-md-6 {{ (($loop->iteration%2) == 0) ? 'order-md-1' : '' }}" data-loop="{{ $loop->iteration }}">
                     <h4>{{ $facility->facilityname }}</h4>
-                    {{ $facility->description }}
+                    {!! $facility->description !!}
                 </div>
             </div>
         @endforeach
