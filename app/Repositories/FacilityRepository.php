c5bc33cc2c2fa7cc0ca37f250bba01dbeaa19349
suhasish86 <suhasish86@gmail.com>
2021-06-24 05:44:59 +0530

    commit 23-06-21-19

--- app/Repositories/FacilityRepository.php
+++ app/Repositories/FacilityRepository.php
@@ -8,12 +8,16 @@ class FacilityRepository implements FacilityRepositoryInterface
 {
     public function get_facility($type = false)
     {
-        if($type){
-            $facility = Facility::where('status',1)->where('facilitytype', $type)->first();
-            // dd($facility);
-            if($facility->image != ''){
-                $facility->image = asset('/storage/uploads/facilityimage/' . $facility->image);
+        if ($type) {
+            $facility = Facility::where('status', 1)->where('facilitytype', $type)->get();
+            if (!empty($facility)) {
+                foreach ($facility as $fc) {
+                    if ($fc->image != '') {
+                        $fc->image = asset('/storage/uploads/facilityimage/' . $fc->image);
+                    }
+                }
             }
+
             return $facility;
         }
         return false;
